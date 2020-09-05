dc13176140982fcb5bc030627ab7b16534c7ace2
Soumik Paul <soumikpaulonly@gmail.com>
2020-09-05 18:28:48 +0530

    spring config server

---
+++ src/main/java/com/kaliber/KaliberConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.kaliber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class KaliberConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaliberConfigServerApplication.class, args);
+	}
+
+}
